b6cbb2b3e35741efd4c901a855a88e7be1e29848
sysuKinthon <qinkai1994@qq.com>
2019-06-18 21:06:14 +0800

    lab-1-3 finish

--- src/mapreduce/schedule.go
+++ src/mapreduce/schedule.go
@@ -1,6 +1,9 @@
 package mapreduce
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 //
 // schedule() starts and waits for all tasks in the given phase (mapPhase
@@ -30,5 +33,34 @@ func schedule(jobName string, mapFiles []string, nReduce int, phase jobPhase, re
 	//
 	// Your code here (Part III, Part IV).
 	//
+	//循环调用call ntasks次,并等待所有的协程都结束
+	var wg sync.WaitGroup
+	for i := 0; i < ntasks; i++ {
+		wg.Add(1)
+		//封装当前任务需要的参数
+		var args DoTaskArgs
+		args.JobName = jobName
+		args.Phase = phase
+		args.TaskNumber = i
+		args.NumOtherPhase = n_other
+		if phase == mapPhase {
+			args.File = mapFiles[i]
+		}
+		//每个task调用一次call
+		go func() {
+			defer wg.Done()
+			for { //放入到无限循环中，只要task没有完成，就一直执行
+				worker := <-registerChan //获取一个就绪态的worker
+				ok := call(worker, "Worker.DoTask", args, nil)
+				if ok { //如果没有成功的话重新开始循环; 如果成功就结束
+					go func() {
+						registerChan <- worker //将worker重新放回，让其可以接受新的task
+					}()
+					break
+				}
+			}
+		}()
+	}
+
 	fmt.Printf("Schedule: %v done\n", phase)
 }
